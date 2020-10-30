f7d8aef89c27ab12766228006da1e1d168b8dad7
Ha-Shamen <35112746+Ha-Shamen@users.noreply.github.com>
2020-10-30 15:23:45 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const TeleBot = require("telebot");
+//instantiate Telebot with our token got in the BtFather
+  const bot = new TeleBot({
+    token: "617229388:AAFtTHLGAvYgSdMOC7_KAVaeRgyAyh1nKp8",
+});
 
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
   console.log(req.body);
+  bot.sendMessage(req.body.message.chat.id, `Hello ${req.body.message.chat.username}`);
 
 //   //instantiate Telebot with our token got in the BtFather
 //   const bot = new TeleBot({
