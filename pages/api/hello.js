4837b28e63879093d7c25af5d0c0454886809896
Ha-Shamen <35112746+Ha-Shamen@users.noreply.github.com>
2020-10-30 15:11:44 +0200

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const TeleBot = require("telebot");
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+//   res.statusCode = 200
+//   res.json({ name: 'John Doe' })
+
+
+  //instantiate Telebot with our token got in the BtFather
+  const bot = new TeleBot({
+    token: "617229388:AAFtTHLGAvYgSdMOC7_KAVaeRgyAyh1nKp8",
+  });
+
+  //our command
+  bot.on(["/start", "/hello"], (msg) => {
+    //all the information about user will come with the msg
+    console.log(msg);
+    bot.sendMessage(msg.from.id, `Hello ${msg.chat.username}`);
+  });
+
+  //start polling
+  bot.start();
 }
